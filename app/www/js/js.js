12aaf81ac2c38abb475506718346f8a12fb3cb76
Taynara <taynaraax0@hotmail.com>
2022-06-09 20:50:25 -0300

    subi

---
+++ app/www/js/js.js
@@ -0,0 +1,7 @@
+function MudarFundo(){
+    let corR = Math.floor(Math.random()* 255);
+    let corG = Math.floor(Math.random()* 255);
+    let corB = Math.floor(Math.random()* 255);
+    let cor = `rgb(${corR} ,${corG}, ${corB})`
+    console
+}
